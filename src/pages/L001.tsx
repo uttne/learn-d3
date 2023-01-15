0879ff0240078add769f5585192daf22c08bbe2c
uttne <uttne.mkr@gmail.com>
2023-01-16 01:25:18 +0900

    基本的な d3.js の使い方を試した

--- src/pages/L001.tsx
+++ src/pages/L001.tsx
@@ -1,49 +1,89 @@
-import { Typography } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useMemo } from "react";
 import * as d3 from "d3";
 import Button from "@mui/material/Button";
 
+/** d3.js で操作するためのチェーンを取得する */
+function getChain<TTag extends keyof ElementTagNameMap, TData>(
+  svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
+  data: TData[],
+  tag: TTag,
+  selector: string
+): d3.Selection<ElementTagNameMap[TTag], TData, SVGSVGElement, unknown> {
+  // 現在のエレメントをセレクトする
+  // 1つ目の型 : select するエレメントの型
+  // 2つ目の型 : data の型
+  const chain = svg
+    .selectAll<ElementTagNameMap[TTag], number[]>(selector)
+    .data(data);
+
+  // 余分なエレメントを削除する
+  chain.exit().remove();
+
+  // まだ追加していないエレメントを追加する
+  const chainAdd = chain.enter().append(tag);
+
+  // 指定のエレメントの更新を行うため現在とまだ追加されていないものをマージ
+  const chainUpdate = chainAdd.merge(chain);
+
+  return chainUpdate;
+}
+
+const svgWidth = 500;
+const svgHeight = 500;
+const dataCount = 5000;
+
 export function L001() {
   const [reloadCount, setReloadCount] = useState(0);
+  const svg = useRef(
+    null as d3.Selection<SVGSVGElement, unknown, HTMLElement, any> | null
+  );
   const canvasRef = useRef<HTMLDivElement>(null);
   // React 18 で StricMode の場合 useEffect が 2回実行されてしまうのでその対応
-  const didEffect = useRef(false);
   useEffect(() => {
-    if (didEffect.current) return;
-    if (canvasRef.current) {
-      canvasRef.current.innerHTML = "";
-    }
-    didEffect.current = true;
-    const svg = d3
+    if (svg.current) return;
+    svg.current = d3
       .select(".canvas")
       .append("svg")
-      .attr("width", 500)
-      .attr("height", 500);
+      .attr("width", svgWidth)
+      .attr("height", svgHeight);
+  }, []);
 
-    svg
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", 0)
-      .attr("width", 10)
-      .attr("height", 10)
-      .attr("fill", "blue");
+  // data の生成
+  const data = useMemo(() => Array(dataCount).map((_, i) => i), []);
 
-    svg
-      .selectAll("rect")
-      .attr("width", 0)
+  useEffect(() => {
+    const s = svg.current;
+    if (!s) return;
+
+    // 指定のエレメントの更新を行うため現在とまだ追加されていないものをマージ
+    const circleUpdate = getChain(s, data, "circle", "circle");
+
+    // ランダム関数の作成
+    const widthRandom = d3.randomUniform(svgWidth);
+    const heightRandom = d3.randomUniform(svgHeight);
+
+    // カラーパレットの作成
+    const color = d3
+      .scaleLinear<string, number, never>()
+      .domain([0, data.length - 1])
+      .range(["red", "blue"]);
+
+    // d3.js で操作
+    circleUpdate
       .transition()
-      .delay(1000)
-      .duration(3000)
-      .ease(d3.easeLinear)
-      .attr("width", 200);
-  }, [reloadCount]);
+      .duration(1000)
+      .attr("cx", (d) => widthRandom())
+      .attr("cy", (d) => heightRandom())
+      .attr("r", 5)
+      .attr("fill", (d, i) => color(i));
+  }, [reloadCount, data]);
+
   return (
     <div className="page-root">
       <div className="canvas" ref={canvasRef} />
       <div className="control">
         <Button
           onClick={() => {
-            didEffect.current = false;
             setReloadCount(reloadCount + 1);
           }}
         >
