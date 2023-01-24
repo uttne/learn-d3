6c3daa83d4eb1a320832b3ee08e8d82d076d9d72
uttne <uttne.mkr@gmail.com>
2023-01-25 01:16:14 +0900

    ドラッグと force を使ったサンプルでオブジェクトの移動を学ぶ

--- src/pages/L002.tsx
+++ src/pages/L002.tsx
@@ -2,31 +2,11 @@ import { useEffect, useRef, useState, useMemo } from "react";
 import * as d3 from "d3";
 import Button from "@mui/material/Button";
 
-/** d3.js で操作するためのチェーンを取得する */
-function getChain<TTag extends keyof ElementTagNameMap, TData>(
-  svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
-  data: TData[],
-  tag: TTag,
-  selector: string
-): d3.Selection<ElementTagNameMap[TTag], TData, SVGSVGElement, unknown> {
-  // 現在のエレメントをセレクトする
-  // 1つ目の型 : select するエレメントの型
-  // 2つ目の型 : data の型
-  const chain = svg
-    .selectAll<ElementTagNameMap[TTag], number[]>(selector)
-    .data(data);
-
-  // 余分なエレメントを削除する
-  chain.exit().remove();
-
-  // まだ追加していないエレメントを追加する
-  const chainAdd = chain.enter().append(tag);
-
-  // 指定のエレメントの更新を行うため現在とまだ追加されていないものをマージ
-  const chainUpdate = chainAdd.merge(chain);
-
-  return chainUpdate;
-}
+/*
+参考
+https://observablehq.com/d/c55a5839a5bb7c73
+https://gist.github.com/mbostock/2990a882e007f8384b04827617752738
+*/
 
 const svgWidth = 500;
 const svgHeight = 500;
@@ -34,16 +14,16 @@ const dataCount = 1000;
 
 export function L002() {
   const [reloadCount, setReloadCount] = useState(0);
-  const svg = useRef(
-    null as d3.Selection<SVGSVGElement, unknown, HTMLElement, any> | null
+  const canvas = useRef(
+    null as d3.Selection<HTMLCanvasElement, unknown, HTMLElement, any> | null
   );
   const canvasRef = useRef<HTMLDivElement>(null);
   // React 18 で StricMode の場合 useEffect が 2回実行されてしまうのでその対応
   useEffect(() => {
-    if (svg.current) return;
-    svg.current = d3
+    if (canvas.current) return;
+    canvas.current = d3
       .select(".canvas")
-      .append("svg")
+      .append("canvas")
       .attr("width", svgWidth)
       .attr("height", svgHeight);
   }, []);
@@ -52,30 +32,65 @@ export function L002() {
   const data = useMemo(() => Array(dataCount).map((_, i) => i), []);
 
   useEffect(() => {
-    const s = svg.current;
-    if (!s) return;
+    const c = canvas.current;
+    if (!c) return;
+    const canvasNode = c.node();
+    if (!canvasNode) return;
+    const context = canvasNode.getContext("2d");
+    if (!context) return;
 
-    // 指定のエレメントの更新を行うため現在とまだ追加されていないものをマージ
-    const circleUpdate = getChain(s, data, "circle", "circle");
+    const radius = 20;
 
-    // ランダム関数の作成
-    const widthRandom = d3.randomUniform(svgWidth);
-    const heightRandom = d3.randomUniform(svgHeight);
+    const circles = d3.range(324).map((i) => ({
+      x: (i % 25) * (radius + 1) * 2,
+      y: Math.floor(1 / 25) * (radius + 1) * 2,
+    }));
 
-    // カラーパレットの作成
-    const color = d3
-      .scaleLinear<string, number, never>()
-      .domain([0, data.length - 1])
-      .range(["red", "blue"]);
+    const drawCircle = (d: { x: number; y: number }) => {
+      context.moveTo(d.x + radius, d.y);
+      context.arc(d.x, d.y, radius, 0, 2 * Math.PI);
+    };
+    const drawCircles = () => {
+      context.clearRect(0, 0, svgWidth, svgHeight);
+      context.save();
+      context.beginPath();
+      circles.forEach(drawCircle);
+      context.fill();
+      context.strokeStyle = "#fff";
+      context.stroke();
+    };
+    const simulation = d3
+      .forceSimulation(circles)
+      .force("collide", d3.forceCollide(radius + 1).iterations(4))
+      .on("tick", drawCircles);
 
-    // d3.js で操作
-    circleUpdate
-      .transition()
-      .duration(1000)
-      .attr("cx", (d) => widthRandom())
-      .attr("cy", (d) => heightRandom())
-      .attr("r", 5)
-      .attr("fill", (d, i) => color(i));
+    const dragsubject = (e: any) => {
+      return simulation.find(e.x, e.y, radius);
+    };
+    const dragstarted = (e: any) => {
+      if (!e.active) simulation.alphaTarget(0.3).restart();
+      e.subject.fx = e.subject.x;
+      e.subject.fy = e.subject.y;
+    };
+    const dragged = (e: any) => {
+      e.subject.fx = e.x;
+      e.subject.fy = e.y;
+    };
+    const dragended = (e: any) => {
+      if (!e.active) simulation.alphaTarget(0);
+      e.subject.fx = null;
+      e.subject.fy = null;
+    };
+
+    // any にしてしまったが型をきちんと確認する
+    const callhandler: any = d3
+      .drag()
+      .container(canvasNode)
+      .subject(dragsubject)
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+    c.call(callhandler);
   }, [reloadCount, data]);
 
   return (
